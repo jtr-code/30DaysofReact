f5a21b69d9689be2b0d012ea6a4d4cceb3237a81
jtr-code <jtrtricks@gmail.com>
2022-11-19 19:52:27 +0530

    Added onChange Event

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,34 +1,36 @@
-
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Forms extends Component {
-
     state = {
-        inputValue:""
-        
-    }
-     
+        inputValue: "",
+    };
+
     handleChange = (event) => {
-        console.log("working");
         this.setState({
-            inputValue: event.target.value 
-        })
-    }
+            inputValue: event.target.value,
+        });
+    };
 
     handleClick = () => {
-        console.log(this.state.inputValue)
-    }
- 
-  render() {
-    return (
-        <form>
-            <h2>Form Component</h2>
-            <input type="text" value={this.state.inputValue} onChange={this.handleChange} name="form" id="" />
-            <button type='button' onClick={this.handleClick}>Submit</button>
-        </form>
+        console.log(this.state.inputValue);
+    };
 
-        
-    )
-  }
+    render() {
+        return (
+            <form>
+                <h2>Form Component</h2>
+                <input
+                    type="text"
+                    value={this.state.inputValue}
+                    onChange={this.handleChange}
+                    name="form"
+                    id=""
+                />
+                <button type="button" onClick={this.handleClick}>
+                    Submit
+                </button>
+            </form>
+        );
+    }
 }
-export default Forms
+export default Forms;
